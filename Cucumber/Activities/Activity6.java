bb4191d87040034523ba4966e0db129397ba89a0
Ipsita-10 <ipsita.bose@ibm.com>
2024-09-17 00:46:04 +0530

    Add files via upload

--- Cucumber/Activities/Activity6.java
+++ Cucumber/Activities/Activity6.java
@@ -1,25 +1,39 @@
-package testRunner;
+package stepDefinitions;
 
-import org.junit.runner.RunWith;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import java.util.List;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-		features = "src/test/java/features",
-		glue = {"stepDefinitions"},
-		tags = "@activity1 or @activity2 or @activity4 or @activity5 or @activity3",
-		publish = true,
-		plugin = {
-			"pretty",
-			"html:src/reports/HTML_Reports.html",
-			"json:src/reports/JSON_Reports.json",
-			"junit:src/reports/XML_Reports.xml"
-			},
-		monochrome = true
-		)
+public class Activity6 extends Base {
 
-public class testRunner {
+    @Given("user is on ToDo list page")
+    public void navigateToPage() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.get("https://v1.training-support.net/selenium/todo-list");
+    }
 
+    @When("User enters the following tasks")
+    public void inputTasks(DataTable inputTasks) {
+        List<String> tasks = inputTasks.asList();
+        driver.findElement(By.xpath("//div[@class = 'ui red bottom attached button']")).click();
+        for (String task : tasks) {
+            driver.findElement(By.id("taskInput")).sendKeys(task);
+            driver.findElement(By.xpath("//button[@class = 'ui blue button']")).click();
+        }
+    }
+
+    @Then("tasks are added")
+    public void verifyResults() {
+        List<WebElement> ele = driver.findElements(By.xpath("//div[@id = 'tasksList']/div"));
+        Assert.assertEquals(3, ele.size());
+    }
 }
