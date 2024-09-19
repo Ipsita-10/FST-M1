342cc0bc77619e94e8c40c50e22ad7f7ebb0111c
Ipsita-10 <ipsita.bose@ibm.com>
2024-09-20 01:06:53 +0530

    Add files via upload

--- Rest_Assured/Activities/Activity1.java
+++ Rest_Assured/Activities/Activity1.java
@@ -1,81 +1,61 @@
 package activities;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
-import org.testng.annotations.BeforeClass;
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-
-import org.testng.annotations.AfterClass;
-
-
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class Activity1 {
-	static String baseUrl;
-	int petId;
-	@BeforeClass
-	  public void beforeClass() {
-		baseUrl = "https://petstore.swagger.io/v2/pet" ;
-	  }
-	
-	//GET https://petstore.swagger.io/v2/pet/{petId}
-		@Test(priority = 2)
-		public void getRequestWithPathParams() {
-			given().
-				baseUri(baseUrl).
-				header("Content-Type", "application/json").
-				pathParam("petId", 77232).
-			when().
-				get("/{petId}").
-			then().
-				statusCode(200).
-				body("status", equalTo("alive")).
-				body("name", equalTo("Riley")).
-				log().all();
-		}
-	
-	//POST https://petstore.swagger.io/v2/pet
-	@Test(priority = 1)
-	public void postRequest() {
-		String reqBody = "{\n"
-				+ "  \"id\": 77232,\n"
-				+ "  \"name\": \"Riley\",\n"
-				+ "  \"status\": \"alive\"\n"
-				+ "}";
-		
-				given().
-					header("Content-Type", "application/json").
-					body(reqBody).
-				when().
-					post(baseUrl).
-				then().
-					statusCode(200).
-					body("id", equalTo(77232)).
-					body("name", equalTo("Riley")).
-					body("status", equalTo("alive")).
-					log().all();
-				;
-				
-	}
-	
-	//DELETE https://petstore.swagger.io/v2/pet/{petId}
-	@Test(priority = 3)
-	public void deleteRequest() {
-	given().
-		header("Content-Type", "application/json").
-		pathParam("petId", 77232).
-	when().
-		delete(baseUrl +"/{petId}").
-	then().
-		statusCode(200).
-		body("message", equalTo(""+petId)).
-		log().all();
-	}
-  
 
-  @AfterClass
-  public void afterClass() {
-  }
+    // Set base URL
+    final static String ROOT_URI = "https://petstore.swagger.io/v2/pet";
 
+    @Test(priority=1)
+    public void addNewPet() {
+        // Create JSON request
+        Map<String, Object> reqBody = new HashMap<>();
+        reqBody.put("id", 2039);
+        reqBody.put("name", "Shifu");
+        reqBody.put("status", "alive");
+
+        Response response =
+                given().contentType(ContentType.JSON)
+                        .body(reqBody)
+                        .when().post(ROOT_URI);
+
+        // Assertion
+        response.then().body("id", equalTo(2039));
+        response.then().body("name", equalTo("Shifu"));
+        response.then().body("status", equalTo("alive"));
+    }
+
+    @Test(priority=2)
+    public void getPetInfo() {
+        Response response =
+                given().contentType(ContentType.JSON)
+                        .when().pathParam("petId", "2039")
+                        .get(ROOT_URI + "/{petId}");
+
+        // Assertion
+        response.then().body("id", equalTo(2039));
+        response.then().body("name", equalTo("Shifu"));
+        response.then().body("status", equalTo("alive"));
+    }
+
+    @Test(priority=3)
+    public void deletePet() {
+        Response response =
+                given().contentType(ContentType.JSON)
+                        .when().pathParam("petId", 2039)
+                        .delete(ROOT_URI + "/{petId}");
+
+        // Assertion
+        response.then().body("code", equalTo(200));
+        response.then().body("message", equalTo("2039"));
+    }
 }
